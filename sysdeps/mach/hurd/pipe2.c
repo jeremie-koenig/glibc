ed4024ccc9698c7a14e087bf2f91642a5b52fe9f
Thomas Schwinge <tschwinge@gnu.org>
2008-12-17 18:11:47 +0100

    pipe2 for GNU Hurd.
    * sysdeps/mach/hurd/pipe2.c: New file, copy from pipe.c.  Evolve it to
    implement __pipe2.
    * sysdeps/mach/hurd/pipe.c (__pipe): Reimplement using __pipe2.
    * sysdeps/mach/hurd/kernel-features.h (__ASSUME_PIPE2): Define.

--- sysdeps/mach/hurd/pipe2.c
+++ sysdeps/mach/hurd/pipe2.c
@@ -18,17 +18,44 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <fcntl-internal.h>
+#include <hurd.h>
 
 /* Create a one-way communication channel (pipe).
    Actually the channel is two-way on the Hurd.
    If successful, two file descriptors are stored in FDS;
    bytes written on FDS[1] can be read from FDS[0].
+   Apply FLAGS to the new file descriptors.
    Returns 0 if successful, -1 if not.  */
 int
-__pipe (int fds[2])
+__pipe2 (int fds[2], int flags)
 {
-  return __pipe2 (fds, 0);
+  int save_errno = errno;
+  int result;
+
+  if (flags & ~(O_CLOEXEC | O_NONBLOCK))
+    return __hurd_fail (EINVAL);
+
+  flags = o_to_sock_flags (flags);
+
+  /* The magic S_IFIFO protocol tells the pflocal server to create
+     sockets which report themselves as FIFOs, as POSIX requires for
+     pipes.  */
+  result = __socketpair (PF_LOCAL, SOCK_STREAM | flags, S_IFIFO, fds);
+  if (result == -1 && errno == EPROTONOSUPPORT)
+    {
+      /* We contacted an "old" pflocal server that doesn't support the
+         magic S_IFIFO protocol.
+	 FIXME: Remove this junk somewhere in the future.  */
+      __set_errno (save_errno);
+      return __socketpair (PF_LOCAL, SOCK_STREAM | flags, 0, fds);
+    }
+
+  return result;
 }
-libc_hidden_def (__pipe)
-weak_alias (__pipe, pipe)
+weak_alias (__pipe2, pipe2)
