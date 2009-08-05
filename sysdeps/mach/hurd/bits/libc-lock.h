3216a609e0ac3e9ba602b663ad643849538c67c3
Thomas Schwinge <tschwinge@gnu.org>
2009-08-05 14:45:11 +0200

    * sysdeps/mach/hurd/bits/libc-lock.h (__rtld_lock_recursive_t): New typedef.

--- sysdeps/mach/hurd/bits/libc-lock.h
+++ sysdeps/mach/hurd/bits/libc-lock.h
@@ -1,5 +1,8 @@
 /* libc-internal interface for mutex locks.  Hurd version using Mach cthreads.
-   Copyright (C) 1996,97,98,2000,01, 2002, 2003 Free Software Foundation, Inc.
+
+   Copyright (C) 1996, 1997, 1998, 2000, 2001, 2002, 2003, 2009 Free Software
+   Foundation, Inc.
+
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -31,6 +34,7 @@ typedef struct
   void *owner;
   int count;
 } __libc_lock_recursive_t;
+typedef __libc_lock_recursive_t __rtld_lock_recursive_t;
 
 #define __libc_lock_owner_self() ((void *) __hurd_threadvar_location (0))
 
